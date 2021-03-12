cc0178b605b756052fd48f615064b902cebced4b
asma.k.patel1@gmail.com <asma.k.patel1@gmail.com>
2021-03-12 09:28:48 +0100

    MessageTable added

---
+++ AzureFunctionsTbleStrg/Model/MessageTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AzureFunctionsTbleStrg.Model
+{
+    public class MessageTable
+    {
+        public string PartitionKey {get;set;}
+        public string  RowKey { get; set; }
+        public double Distance { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string DeviceId { get; set; }
+        public DateTime epochTime { get; set; }
+        public string SchoolName { get; set; }
+        public string StudentName { get; set; }
+
+
+    }
+}
--- AzureFunctionsTbleStrg/SaveToTableStorage.cs
+++ AzureFunctionsTbleStrg/SaveToTableStorage.cs
@@ -6,6 +6,9 @@ using Microsoft.Azure.EventHubs;
 using System.Text;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
+using AzureFunctionsTbleStrg.Model;
+using Newtonsoft.Json;
+using System;
 
 namespace AzureFunctionsTbleStrg
 {
@@ -14,9 +17,45 @@ namespace AzureFunctionsTbleStrg
         private static HttpClient client = new HttpClient();
 
         [FunctionName("SaveToTableStorage")]
-        public static void Run([IoTHubTrigger("messages/events", Connection = "IotHubConnection")]EventData message, ILogger log)
+        [return: Table("sensor")]
+        public static MessageTable Run([IoTHubTrigger("messages/events", Connection = "IotHubConnection")]EventData message, ILogger log)
         {
-            log.LogInformation($"C# IoT Hub trigger function processed a message: {Encoding.UTF8.GetString(message.Body.Array)}");
+            try
+            {
+                var payload = JsonConvert.DeserializeObject<MessageTable>(Encoding.UTF8.GetString(message.Body.Array));
+               // payload.PartitionKey = message.Properties["type"].ToString(); 
+               // payload.RowKey = Guid.NewGuid().ToString();
+                var _deviceId = message.SystemProperties["iothub-connection-device-id"].ToString();
+               // var _deviceType = message.Properties["type"].ToString();
+                var _schoolName = message.Properties["SchoolName"].ToString();
+                var _studentName = message.Properties["StudentName"].ToString();
+
+                DateTimeOffset dateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(message.Properties["epochTime"].ToString()));
+                DateTime _measurementTime = dateTimeOffset.DateTime;
+
+                var _table = new MessageTable()
+                {
+                    PartitionKey = message.Properties["type"].ToString(),
+                    RowKey = Guid.NewGuid().ToString(),
+                    DeviceId = _deviceId,
+                    epochTime = _measurementTime,
+                    SchoolName = _schoolName,
+                    StudentName = _studentName,
+                    Distance = payload.Distance,
+                    Latitude = payload.Latitude,
+                    Longitude = payload.Longitude,
+                };
+                var _tablejson = JsonConvert.SerializeObject(_table);
+                log.LogInformation($"Measurement was saved to Table Storage, Message:: {_tablejson}");
+
+                return _table;
+
+               }
+            catch
+            {
+                log.LogInformation("Failed to Deserialize message. No data was save to Table Storage");
+            }
+            return null;
         }
     }
 }
