15b22fe7dcb3e6453d02d5d2e33366e88c3c3c85
asma.k.patel1@gmail.com <asma.k.patel1@gmail.com>
2021-03-12 14:06:50 +0100

    HTMLtableStorage

---
+++ WebsitesTabStr/script.js
@@ -0,0 +1,17 @@
+let tabledata = document.getElementById('tabledata')
+
+//setInterval(() => {
+//tabledata.innerHTML = "";
+
+fetch("https://iot20-func.azurewebsites.net/api/GetDataFrnTbleStrg?code=a/uUH3sAzjEy4WSam7muaOAv53CMJsXap84QIEa0PYLhadqSGOXJ4A==")
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data);
+
+        for (let row of data) {
+            // tabledata.innerHTML += `<tr><td>${row.rowKey}</td><td>${row.deviceId}</td><td>${row.temperature}</td><td>${row.humidity}</td><td>${row.msgCreateTime}</td>`
+            tabledata.innerHTML += `<tr><td>${row.rowKey}</td><td>${row.deviceId}</td><td>${row.distance}</td><td>${row.latitude}</td><td>${row.longitude}</td>`
+
+        }
+    })
+    //}, 5000);
